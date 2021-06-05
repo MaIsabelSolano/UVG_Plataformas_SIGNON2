9bef8555664b5b88ffe2c08c14de74e212d13218
MaIsabelSolano <misolanob@gmail.com>
2021-06-05 12:52:22 -0600

    clase main

--- app/src/main/java/com/uvg/signon2/MainActivity.kt
+++ app/src/main/java/com/uvg/signon2/MainActivity.kt
@@ -1,11 +1,71 @@
 package com.uvg.signon2
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.api.ApiException
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.GoogleAuthProvider
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var auth:FirebaseAuth
+    private lateinit var googlesSignInClient: GoogleSignInClient
+    private val TAG: String = "Sign In"
+    private val RC_SIGN_IN: Int = 1
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        auth = FirebaseAuth.getInstance()
+
+        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken("").requestEmail().build()
+
+        googlesSignInClient = GoogleSignIn.getClient(this,gso)
+
+        var btnIniciar = findViewById<Button>(R.id.btnIniciarSesion)
+        btnIniciar.setOnClickListener { signIn() }
+    }
+
+    private fun signIn(){
+        val signInIntent = googlesSignInClient.signInIntent
+        startActivityForResult(signInIntent,RC_SIGN_IN)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode == RC_SIGN_IN){
+            val task = GoogleSignIn.getSignedInAccountFromIntent(data)
+            try {
+                val account = task.getResult(ApiException::class.java)!!
+                Log.d(TAG,"firebaseAutWithGoogle"+account.id)
+                firebaseAuthWithGoogle(account.idToken!!)
+            }
+            catch (e: ApiException){
+                Log.w(TAG,"Google sign in failed")
+            }
+        }
+    }
+
+    private fun firebaseAuthWithGoogle(idToken:String){
+        val credential = GoogleAuthProvider.getCredential(idToken,null)
+        auth.signInWithCredential(credential)
+            .addOnCompleteListener(this){task ->
+                if (task.isSuccessful){
+                    Log.d(TAG,"signInWithCredentials:Success")
+                }
+                else {}
+            }
     }
 }
+
+
+
