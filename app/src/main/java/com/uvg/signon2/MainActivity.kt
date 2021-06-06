930bc0ca37e4a57197c8092436ef72f03f5e21ea
MaIsabelSolano <misolanob@gmail.com>
2021-06-05 20:58:31 -0600

    ...

--- app/src/main/java/com/uvg/signon2/MainActivity.kt
+++ app/src/main/java/com/uvg/signon2/MainActivity.kt
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.widget.Button
+import android.widget.Toast
 import com.google.android.gms.auth.api.signin.GoogleSignIn
 import com.google.android.gms.auth.api.signin.GoogleSignInClient
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions
@@ -18,7 +19,8 @@ class MainActivity : AppCompatActivity() {
     private lateinit var auth:FirebaseAuth
     private lateinit var googlesSignInClient: GoogleSignInClient
     private val TAG: String = "Sign In"
-    private val RC_SIGN_IN: Int = 1
+    private val RC_SIGN_IN: Int = 120
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -27,7 +29,9 @@ class MainActivity : AppCompatActivity() {
         auth = FirebaseAuth.getInstance()
 
         val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
-            .requestIdToken("AIzaSyDfklgGP9B6w4AKSP8c52IdG0PknBl8wEg").requestEmail().build()
+            .requestIdToken("AIzaSyDfklgGP9B6w4AKSP8c52IdG0PknBl8wEg")
+            .requestEmail()
+            .build()
 
         googlesSignInClient = GoogleSignIn.getClient(this,gso)
 
@@ -39,30 +43,48 @@ class MainActivity : AppCompatActivity() {
         val signInIntent = googlesSignInClient.signInIntent
         startActivityForResult(signInIntent,RC_SIGN_IN)
 
+        Toast.makeText(this,"Sign in",Toast.LENGTH_SHORT).show()
+        //val MapaIntent = Intent(this,Mapa::class.java)
+        //startActivity(MapaIntent)
+
     }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
 
+        Toast.makeText(this," Result",Toast.LENGTH_SHORT).show()
+
         if (requestCode == RC_SIGN_IN){
             val task = GoogleSignIn.getSignedInAccountFromIntent(data)
+            Toast.makeText(this,"request code",Toast.LENGTH_SHORT).show()
             try {
+                Toast.makeText(this,"Success0",Toast.LENGTH_SHORT).show()
                 val account = task.getResult(ApiException::class.java)!!
                 Log.d(TAG,"firebaseAutWithGoogle"+account.id)
                 firebaseAuthWithGoogle(account.idToken!!)
+                Toast.makeText(this,"Success1",Toast.LENGTH_SHORT).show()
+
             }
             catch (e: ApiException){
-                Log.w(TAG,"Google sign in failed")
+                Log.w(TAG,"Google sign in failed",e)
+                Toast.makeText(this,"fail",Toast.LENGTH_SHORT).show()
+                //val MapaIntent = Intent(this,Mapa::class.java)
+                //startActivity(MapaIntent)
             }
         }
     }
 
     private fun firebaseAuthWithGoogle(idToken:String){
+        Toast.makeText(this,"Success00",Toast.LENGTH_SHORT).show()
         val credential = GoogleAuthProvider.getCredential(idToken,null)
         auth.signInWithCredential(credential)
             .addOnCompleteListener(this){task ->
                 if (task.isSuccessful){
                     Log.d(TAG,"signInWithCredentials:Success")
+                    Toast.makeText(this,"Success2",Toast.LENGTH_SHORT).show()
+                    val MapaIntent = Intent(this,Mapa::class.java)
+                    startActivity(MapaIntent)
+                    //finish()
                 }
                 else {
                     Log.w(TAG,"SignIInWithCredentials:Failure",task.exception)
--- app/src/main/java/com/uvg/signon2/Mapa.kt
+++ app/src/main/java/com/uvg/signon2/Mapa.kt
@@ -1,11 +1,63 @@
 package com.uvg.signon2
 
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
 
-class Mapa : AppCompatActivity() {
+
+class Mapa : AppCompatActivity(), OnMapReadyCallback {
+    /*
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_mapa)
     }
+
+     */
+    private lateinit var map: GoogleMap
+    private val LOCATIOM_PERMISSION_REQ = 1
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        createFragment()
+    }
+
+    private fun createFragment(){
+        val mapFragment = supportFragmentManager.findFragmentById(R.id.map) as SupportMapFragment
+        mapFragment.getMapAsync(this)
+    }
+
+    override fun onMapReady(googleMap: GoogleMap){
+        map = googleMap
+        createMarker()
+    }
+
+    private fun getLocation(){
+        if (ContextCompat.checkSelfPermission(this,android.Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED){
+            map.isMyLocationEnabled = true
+        }
+        else {
+            ActivityCompat.requestPermissions(this, arrayOf(android.Manifest.permission.ACCESS_FINE_LOCATION), LOCATIOM_PERMISSION_REQ)
+        }
+    }
+
+    private fun createMarker(){
+        val coordinates = LatLng(14.603959251809565, -90.48924800330377)
+        val marker = MarkerOptions().position(coordinates).title("UVG")
+        map.addMarker(marker)
+        map.animateCamera(
+            CameraUpdateFactory.newLatLngZoom(coordinates,18f),
+            4000,
+            null
+        )
+    }
+
 }
